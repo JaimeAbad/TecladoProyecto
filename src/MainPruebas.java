41bd9732c18eac6aed4c7183edf0d196ff43a778
JaimeAF <aquileseeb@gmail.com>
2018-11-26 13:19:57 +0100

    Commit8
    Pruebas manuales en eclipse

--- src/MainPruebas.java
+++ src/MainPruebas.java
@@ -1,39 +1,72 @@
+
+
 public class MainPruebas {
 
 	public static void main(String[] args) {
 
-		String b1,b2,b3;
-		b1 = "elige un colors";
-		b2 = "ROJO";
-		b3 = "ASUL";
-//		boolean color;
-		byte z = 6;
+
 		
-//		System.out.printf("%s",b1);
-//		System.out.printf("\n  1.- %s",b2);
-//		System.out.printf("\n  2.- %s",b3);
-		
-//		color  = Teclado.leerBoolean("color?", "rojo", b3);
-//		System.out.println(color);
-		
-		
-		System.out.println();
-		
-//		System.out.println(b2.equals(b3));
-//		byte x;
-//		System.out.println("Introduce un byte");
-//		x = Teclado.leerByte();
-//		System.out.println(x);
-		
-		Teclado.Comparacion(z, Teclado.ES.MAYOR);
-		
-//		b1 = Teclado.leerCadena();
-//		System.out.println(b1);
-		
-		char c;
-		
+		System.out.println("PRUEBAS VALIDAS LEER CARACTER");
+		char c,c2;
+		int c1;
+		System.out.println("Introduce un caracter: ");
 		c = Teclado.leerChar();
+		System.out.println("Caracter-numero(Mostrara el codigo unicode en decimal de dicho numero) Introduce un numero cualquiera : ");
+		c1 = Teclado.leerChar();
+		System.out.println("Caracter-cadena (Introduce una cadena): ");
+		c2 = Teclado.leerChar();
 		System.out.println(c);
+		System.out.println(c1);
+		System.out.println(c2);
+		
+		System.out.println("PRUEBAS VALIDAS LEER CADENA");
+		
+		String s1,s2,s3,s4,s5,s6;
+		
+		System.out.println("Cadena (Introduce hola): ");
+		s1 = Teclado.leerCadena();
+		System.out.println("Introduce un caracter (a, e i ó P): ");
+		s2 = Teclado.leerCadena();
+		System.out.println("Introduce un numero entero: ");
+		s3 = Teclado.leerCadena();
+		System.out.println("Introduce 0: ");
+		s4 = Teclado.leerCadena();
+		System.out.println("Introduce un numero decimal: ");
+		s5 = Teclado.leerCadena();
+		
+		System.out.println("PRUEBAS NO VALIDAS LEER CADENA");
+		
+		System.out.println("Introduce cadena vacia(da intro)");
+		s6 = Teclado.leerCadena();
+		
+		
+		
+		
+		
+		
+		System.out.println("PRUEBAS VALIDAS ");
+		System.out.println("PRUEBAS VALIDAS ");
+		System.out.println("PRUEBAS VALIDAS ");
+		System.out.println("PRUEBAS VALIDAS ");
+		System.out.println("PRUEBAS VALIDAS ");
+		System.out.println("PRUEBAS VALIDAS ");
+		
+		System.out.println("Introduce una cadena: ");
+		System.out.println("Introduce ");
+		System.out.println("Introduce ");
+		System.out.println("Introduce ");
+		System.out.println("Introduce ");
+		System.out.println("Introduce ");
+		System.out.println("Introduce ");
+		System.out.println("Introduce ");
+		System.out.println("Introduce ");
+		System.out.println("Introduce ");
+		System.out.println("Introduce ");
+		System.out.println("Introduce ");
+		System.out.println("Introduce ");
+		
+		
+		
 	}
 
 }
